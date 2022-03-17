cf6bd6e7a23d328799fdb386e688f58b3cebbb08
Glitchii <hello@tempfile.site>
2022-03-17 11:40:50 -0700

    Update builder.config.js

--- builder.config.js
+++ builder.config.js
@@ -1,13 +1,13 @@
 // URL options can override the options below.
 // Options set through the menu can override both.
 options = {
-    username:           "Discord Bot",
-    avatar:             `https://cdn.discordapp.com/embed/avatars/${Math.floor(Math.random() * 6)}.png`,
+    username:           'Discord Bot',
+    avatar:             'https://cdn.discordapp.com/embed/avatars/1.png',
     verified:           false,
     noUser:             false,
     hasParam:           false,
     dataSpecified:      false,
-    guiTabs:            "author,description",
+    guiTabs:            'author,description',
     useJsonEditor:      false,
     reverseColumns:     false,
     onlyEmbed:          false,
